f58c2ef91e05a33be8cd44fb8a9f8a4e6b48c727
Vidhi <srivastavavidhi29@gmail.com>
2023-08-20 17:29:51 +0530

    Implemented Random function

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,14 @@
+import random
+
+def Randoms(Words):
+    res = key, val = random.choice(list(Words.items()))
+    print(res[0])
+    choose = list(res[1])
+    take = random.choice(choose);
+    l = len(take)
+    s = '_ '
+    print(l * s, "\n")
+
 Words = {'COUNTRY': ['India', 'USA', 'Italy', 'New Zealand', 'Russia', 'Germany', 'Mexico', 'China', 'Canada', 'Japan'],
         'DAIRY': ['Milk', 'Butter', 'Cream', 'Cheese', 'Yoghurt', 'Ice-Cream', 'Ghee', 'Custard', 'Whipped Cream', 'Buttermilk'],
         'FRUIT': ['Apple', 'Banana', 'Papaya',	'Peach', 'Pomegranate',	'Pineapple', 'Raspberries', 'Strawberries', 'Watermelon', 'Mango'],
@@ -6,3 +17,5 @@ Words = {'COUNTRY': ['India', 'USA', 'Italy', 'New Zealand', 'Russia', 'Germany'
         'PROFESSION': ['Teacher', 'Dentist', 'Physician', 'Architect', 'Lawyer', 'Software Developer', 'Artist', 'Police officer', 'Scientist', 'chef'],
         'PLANET': ['Earth', 'Jupiter', 'Saturn', 'Venus', 'Mars', 'Mercury', 'Neptune', 'Uranus'],
         'BRAND': ['Apple', 'Nike', 'Adidas', 'Google', 'Microsoft', 'Amazon', 'BMW', 'IBM', 'Starbucks', 'Disney']}
+
+Randoms(Words)
