31fef328c18a38419492db02d544a8589e122a59
Vidhi <srivastavavidhi29@gmail.com>
2023-08-22 12:27:24 +0530

    Saving right letter chooses by user

--- Hangman.py
+++ Hangman.py
@@ -1,50 +1,56 @@
 import random
 
-def Hangman(count):
+def Hangman(count, take):
     if(count == 1):
         print('___________________\n|\n|\n|\n|\n|\n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 2):
         print('___________________\n|     |\n|\n|\n|\n|\n|\n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 3):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|\n|\n|\n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 4):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|     |\n|\n|\n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 5):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|     |\n|    /|\n|   / |\n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 6):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|     |\n|    /|\ \n|   / | \ \n|\n|')
-        print("Try Again")
+        print("\nTry Again")
     elif(count == 7):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|     |\n|    /|\ \n|   / | \ \n|    /\n|   /')
-        print("Try Again")
+        
     elif(count == 8):
         print('___________________\n|     |\n|   .;;;.\n|   (*_*)\n|     |\n|    /|\ \n|   / | \ \n|    / \ \n|   /   \ ')
-        print("You Lose :(")
+        print("\nYOU LOSE :(\n the write word was " + take)
 
         
-def guess(take , s):
+def guess(take):
+    s = '_'
+    b = s * len(take);
+    b = list(b)
     count = 0
     while(count != 8):
         choose = input("Guess letter or whole word: ").lower()
         low = take.lower();
+        k = list(low)
         i = low.find(choose)
-        if(choose == low):
-            print("You win!!!\n" + choose + " is a right word")
+        if(choose == low or b == k):
+            print("\nYOU WIN!!!\n" + choose + " is a right word")
             break
-
         if(i != -1):
-            first = s * i
-            last = s * (len(low) - (i + len(choose)))
-            print(first + choose + last)   
+            j = 0
+            while(j != len(choose)):
+                b[i] = choose[j]
+                j += 1
+                i += 1
+            print(b)
         else:
             count += 1
             if(count < 9):
-                Hangman(count)
+                Hangman(count, take)
 
 def Randoms(Words):
     res = key, val = random.choice(list(Words.items()))
@@ -55,7 +61,7 @@ def Randoms(Words):
     s = '_  '
     blank = l * s;
     print(blank, "\n")
-    guess(take, s)
+    guess(take)
     
     
 
